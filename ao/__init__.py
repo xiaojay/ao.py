949c02627cbb1c93e7e377f363d7e11ff11fc25c
txohyeah <wycxiao@126.com>
2024-05-18 11:44:23 +0800

    get message from su

--- ao/__init__.py
+++ ao/__init__.py
@@ -1,9 +1,12 @@
 import requests
 import arseeding
+import time
+from datetime import datetime, timedelta
 
 SDK = 'ao.py'
 MU = 'https://mu.ao-testnet.xyz'
 CU = 'https://cu.ao-testnet.xyz'
+SU = 'https://su-router.ao-testnet.xyz'
 SCHEDULER = '_GQ33BkPtZrqxA84vM8Zk-N2aO0toNNu_C-l-rawrBA'
 
 def send_message(singer, pid, anchor, tags, data='', mu=MU, timeout=5):
@@ -18,7 +21,7 @@ def send_message(singer, pid, anchor, tags, data='', mu=MU, timeout=5):
     return b.id, requests.post(mu, data=b.binary, headers={'Content-Type': 'application/octet-stream'}, timeout=timeout).json()
 
 
-def dry_run(signer, pid, anchor, tags, data='', cu=CU, timeout=30):
+def dry_run(signer, pid, anchor, tags, data='', cu=CU, timeout=60):
     default_tags = {
         'Data-Protocol': 'ao',
         'Variant': 'ao.TN.1',
@@ -51,9 +54,32 @@ def spawn_process(singer, module, anchor, tags, data='', mu=MU, scheduler=SCHEDU
     b = arseeding.BundleItem(singer, '', anchor, default_tags, data)
     return b.id, requests.post(mu, data=b.binary, headers={'Content-Type': 'application/octet-stream'}).json()
 
-def get_result(pid, message_id, cu=CU):
-    return requests.get(f'{cu}/result/{message_id}?process-id={pid}').json()
+def get_result(pid, message_id, timeout=60, cu=CU):
+    return requests.get(f'{cu}/result/{message_id}?process-id={pid}', timeout=timeout).json()
 
-def send_and_get(singer, pid, anchor, tags, data='', mu=MU, cu=CU):
+def send_and_get(singer, pid, anchor, tags, data='', timeout=60, mu=MU, cu=CU):
     mid, _ = send_message(singer, pid, anchor, tags, data, mu)
-    return mid, get_result(pid, mid, cu)
+    return mid, get_result(pid, mid, timeout=timeout, cu=cu)
+
+def get_latest_messages(pid, time_offset, timeout = 60, su=SU):
+    now = datetime.now()
+    past_time = now - timedelta(seconds=time_offset)
+    past_timestamp = int(time.mktime(past_time.timetuple())) * 1000
+    return get_messages(pid, from_timestamp = past_timestamp, timeout = timeout, su=su)
+
+def get_messages(pid, from_timestamp = None, to_timestamp = None, timeout = 60, su=SU):
+    url = f"{su}/{pid}"
+    param = ""
+    if from_timestamp is not None:
+        param = param + f"?from={from_timestamp}"
+    
+    if to_timestamp is not None:
+        param = param + f"?to={to_timestamp}"
+
+    if param != "":
+        url = url + param
+
+    return requests.get(url, timeout=timeout).json()
+
+def get_message(pid, mid, timeout = 60, su=SU):
+    return requests.get(f"{su}/{mid}?process-id={pid}", timeout=timeout).json()
---
+++ ao/su_messages.py
@@ -0,0 +1,90 @@
+import json
+
+
+class PageInfo:
+    def __init__(self, has_next_page):
+        self.has_next_page = has_next_page
+
+class Tag:
+    def __init__(self, name, value):
+        self.name = name
+        self.value = value
+    
+
+class Message:
+    def __init__(self, id, tags=None, data=None, **kargs):
+        self.id = id
+        self.tags = [Tag(**tag) for tag in tags] if tags else []
+        self.data = data
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'tags': [tag.__dict__ for tag in self.tags],
+            'data': self.data
+        }
+    
+    def has_tag(self, tagname, tagvalue):
+        return any(tag.name == tagname and tag.value == tagvalue for tag in self.tags)
+    
+    def get_tagvalue(self, tagname):
+        for tag in self.tags:
+            if tag.name == tagname:
+                return tag.value
+        return None
+
+    def __str__(self):
+        return json.dumps(self.to_dict(), indent=4)
+
+class Node:
+    def __init__(self, message):
+        self.message = Message(**message)
+
+class Edge:
+    def __init__(self, node, cursor):
+        self.node = Node(node['node']['message'])
+        self.cursor = cursor
+    
+
+class SuResponse:
+    def __init__(self, page_info, edges):
+        self.page_info = PageInfo(**page_info)
+        self.edges = [Edge(edge, edge['cursor']) for edge in edges]
+    
+    def has_messages(self):
+        return len(self.edges) > 0
+    
+    def get_latest_message(self, tagname = None, tagvalue = None):
+        if tagname == None:
+            if not self.has_messages():
+                return None
+            return self.edges[len(self.edges) - 1].node.message
+        else:
+            edges_len = len(self.edges)
+
+            for i in range(edges_len):
+                # 返回的数据就是倒叙排列的
+                if self.edges[edges_len - i - 1].node.message.has_tag(tagname, tagvalue):
+                    return self.edges[edges_len - i - 1].node.message
+                
+            return None
+    
+    def get_latest_balances(self, from_process):
+        edges_len = len(self.edges)
+        for i in range(edges_len):
+            msg = self.edges[edges_len - i - 1].node.message
+            if msg.get_tagvalue("Action") == None and msg.get_tagvalue("From-Process") == from_process:
+                balances_dict = json.loads(msg.data)
+                if int(balances_dict[from_process]) > 100000:
+                    return balances_dict
+        return None
+    
+    def get_latest_balance(self, from_process, me_id):
+        balances_dict = self.get_latest_balances(from_process)
+        if balances_dict == None:
+            return None
+        else:
+            return balances_dict[me_id]
+
+def parse(json):
+    return SuResponse(json['page_info'], json['edges'])
